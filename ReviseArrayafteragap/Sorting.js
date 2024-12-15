99fa3a907afba976bca6c7cbcc904dd059c67307
kanav <kanavrana91@gmail.com>
2024-12-15 10:21:28 +0530

    revise Sorting

---
+++ ReviseArrayafteragap/Sorting.js
@@ -0,0 +1,15 @@
+let arr = [9, 30, 2, 63, 55, 1];
+
+const Sorting = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+};
+console.log(Sorting(arr));
