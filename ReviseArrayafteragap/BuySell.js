8e1b336e794344560d7f379b35ae7c9399127a2b
kanav789 <kanavrana91@gmail.com>
2025-01-06 10:04:23 +0530

    better solution of bestbuy

--- ReviseArrayafteragap/BuySell.js
+++ ReviseArrayafteragap/BuySell.js
@@ -1,20 +1,38 @@
 // Brute force approach
+// var maxProfit = function (prices) {
+//   let maxProfit = 0;
+
+//   for (let i = 0; i < prices.length; i++) {
+//     let currprofit = 0;
+//     for (let j = i + 1; j < prices.length; j++) {
+//       if (prices[j] > prices[i]) {
+//         currprofit = prices[j] - prices[i];
+//       }
+//       maxProfit = Math.max(currprofit, maxProfit);
+//     }
+//   }
+
+//   return maxProfit;
+// };
+
+// Better solution
+
 var maxProfit = function (prices) {
   let maxProfit = 0;
-
-  for (let i = 0; i < prices.length; i++) {
-    let currprofit = 0;
-    for (let j = i + 1; j < prices.length; j++) {
-      if (prices[j] > prices[i]) {
-        currprofit = prices[j] - prices[i];
-      }
-      maxProfit = Math.max(currprofit, maxProfit);
+   let bestbuy=prices[0]
+  for (let i = 1; i < prices.length; i++) {
+   
+    if(maxProfit<prices[i])
+    {
+      maxProfit=Math.max(maxProfit,prices[i]-bestbuy)
+    }
+    if(prices[i]<bestbuy)
+    {
+      bestbuy=prices[i]
     }
   }
 
   return maxProfit;
 };
-
-// Better solution
 
 console.log(maxProfit([7, 6, 5, 4, 3, 2, 1]));
