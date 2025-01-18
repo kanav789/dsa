617f03e4fae07926b3a30463b3ddf25997047bc9
kanav789 <kanavrana91@gmail.com>
2025-01-18 13:25:06 +0530

    sorting algorithm

---
+++ Sort/selection.js
@@ -0,0 +1,15 @@
+// selection sort algorithm
+let arr = [5, 4, 3, 2, 1];
+let n = arr.length;
+for (let i = 0; i < n - 1; i++) {
+  let minIndex = i;
+  for (let j = i + 1; j < n; j++) {
+    if (arr[j] < arr[minIndex]) {
+      minIndex = j;
+    }
+  }
+  let temp = arr[i];
+  arr[i] = arr[minIndex];
+  arr[minIndex] = temp;
+}
+console.log(arr);
