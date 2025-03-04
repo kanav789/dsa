950e410b97b1bfe0d6146a1b196a2323c35b49de
kanav789 <kanavrana91@gmail.com>
2025-03-05 00:37:01 +0530

    how to check special char

---
+++ String/SpecialChar.js
@@ -0,0 +1,17 @@
+// check  weather the string has special character or no`
+const str='@'
+const ch=str.toLowerCase()
+console.log(ch)
+if( ch>='a' && ch<='z' || ch>='A' && ch<='Z' || ch>='0' && ch<='9'){
+console.log(false) 
+}
+    else{
+      console.log(true) 
+    }
+
+
+
+
+
+
+
