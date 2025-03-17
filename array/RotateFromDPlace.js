d12e78f008dc02942bbd56cafc51634da1914d9b
kanav789 <kanavrana91@gmail.com>
2025-03-17 23:58:32 +0530

    Rotate the elements from D place

---
+++ array/RotateFromDPlace.js
@@ -0,0 +1,17 @@
+// if i have to rotate the elements from d place 
+
+// function RotateFromDPlace(arr,d){
+//     let n=arr.length;   
+//     let temp=[];
+//     let temp2=[];
+//     for(let i=0;i<n;i++){
+//         if(i<=d){
+//             temp.push(arr[i]);
+//         }else{
+//             temp2.push(arr[i]);
+//         }
+//     }
+//     return temp2.concat(temp);
+
+// }
+// console.log(RotateFromDPlace([1,2,3,4,5,6,7],3)); // [3,4,5,6,7,1,2]
